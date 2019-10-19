4166384b9c39d43242a316fe5caa34c0d00e6087
Robbie Van Gorkom <robbie.van.gorkom@protonmail.com>
2019-10-19 07:04:14 -0700

    Fixing all the weird issues with travis build.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,17 +6,17 @@ cd "$SELF_DIR/.."
 
 VENV_DIR="venv"
 PYTHON_BIN="python3"
-
-BLACK_ARGS=""
-if [[ -n "${CI:-}" ]]; then
-  BLACK_ARGS="--check"
-fi
+FORMAT_PYTHON_VERSION="Python 3.7"
 
 if ! [[ `which "$PYTHON_BIN"` ]]; then
   echo "Error: '$PYTHON_BIN' is not in your path."
   exit 1
 fi
 
+CODE_FORMATTING_ENABLED=$("$PYTHON_BIN" --version | grep -Ec "^$FORMAT_PYTHON_VERSION" || true)
+
+echo
+echo "===Settting up venv==="
 # Not sure why I couldn't use "if ! [[ `"$PYTHON_BIN" -c 'import venv'` ]]" below. It just never worked when venv was
 # present.
 VENV_NOT_INSTALLED=$("$PYTHON_BIN" -c 'import venv' 2>&1 | grep -ic ' No module named' || true)
@@ -28,26 +28,46 @@ fi
 if ! [[ -e "$VENV_DIR" ]]; then
   echo "Creating venv."
   "$PYTHON_BIN" -m venv "$VENV_DIR"
+else
+  echo Using existing venv.
 fi
 
 if ! [[ `env | grep VIRTUAL_ENV` ]]; then
   echo "Entering venv."
   source "$VENV_DIR/bin/activate"
+else
+  echo Already in venv.
 fi
 
-echo "Installing build dependencies."
-pip install -r "$SELF_DIR/../requirements.txt"
 
-echo "Running module dependencies."
+echo
+echo "===Installing dependencies==="
 python setup.py install
 
-echo "Formatting code."
-python setup.py format
 
-echo "Running tests."
+echo
+echo "===Setting up black==="
+# This runs  reliably on python 3.7, haven't testest python 3.6 and black won't install on python 3.5. So we're
+# conditionally running these checks. Any bad formatting will be cause in the 3.7 build in CI.
+if [[ "$CODE_FORMATTING_ENABLED" > "0" ]]; then
+  # Run check only on ci builds. Format code on local builds.
+  BLACK_ARGS=""
+  if [[ -n "${CI:-}" ]]; then
+    BLACK_ARGS="--check"
+  fi
+
+  echo "Installing black."
+  pip install setuptools-black==0.1.4
+
+  echo "Formatting code with args '$BLACK_ARGS'."
+  python setup.py format $BLACK_ARGS
+else
+  echo "Skipping code format checks as black doesn't work well python versions less than $FORMAT_PYTHON_VERSION."
+fi
+
+
+echo
+echo "===Running tests==="
 python setup.py test
-
-echo "Running pylint";
-python setup.py lint
 
 echo "Build complete."
